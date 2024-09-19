5413b51e714966c01de531e397a832b7f0b91d1f
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-19 15:01:41 +0700

    added account in my bank account

--- payooJS/paysoo.js
+++ payooJS/paysoo.js
@@ -1,8 +1,8 @@
-document.getElementById('Login-btn').addEventListener('click', function(event){
-    event.preventDefault();
+// document.getElementById('Login-btn').addEventListener('click', function(event){
+//     event.preventDefault();
 
-    const inputPhone = document.getElementById('input-phone-field').value;
-    const inputPin = document.getElementById('input-pin-field').value;
+//     const inputPhone = document.getElementById('input-phone-field').value;
+//     const inputPin = document.getElementById('input-pin-field').value;
 
     // if(typeof inputPhone !== "number" && typeof inputPin !== "number"){
     //     alert('Invalid Input');
@@ -10,11 +10,29 @@ document.getElementById('Login-btn').addEventListener('click', function(event){
     // const phoneNumber = parseFloat(inputPhone);
     // const pinNumber = parseFloat(inputPin);
 
-    if(inputPhone === '8' && inputPin === '1234'){
-        console.log('your in home page');
+//     if(inputPhone === '8' && inputPin === '1234'){
+//         console.log('your in home page');
+//         window.location.href = '/Home.html'
+//     }
+//     else{
+//         alert("Invalid Input")
+//     }
+// })
+
+// ================= Practice part ================== //
+
+document.getElementById('Login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const inputNumber = document.getElementById('input-phone-field').value;
+    const inputPinNumer = document.getElementById('input-pin-field').value;
+
+    if(inputNumber === '8' && inputPinNumer === '1234'){
+        console.log('your are loggin to home page');
         window.location.href = '/Home.html'
+
     }
     else{
-        alert("Invalid Input")
+        alert('Invalid Input');
     }
 })
